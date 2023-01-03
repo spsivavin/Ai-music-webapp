81da41b7f619d4081d52651dc0200aa9e74c6a12
spsivavin <103201497+spsivavin@users.noreply.github.com>
2023-01-03 20:07:31 +0530

    Add files via upload

---
+++ AI_Music_webapp/main.js
@@ -0,0 +1,20 @@
+music = "";
+music1 = "";
+
+function preload()
+{
+    music = loadSound("music.mp3");
+    music1 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
